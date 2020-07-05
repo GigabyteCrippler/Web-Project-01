3d2fb2fbc45c3606ddc073034da579b2945dd4df
GigabyteCrippler <jeffbonimississo@gmail.com>
2020-07-05 12:10:35 +0000

    navbar introduced

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Footer from './Footer';
+import {NavBar} from './NavBar';
 
 function App() {
   return (
     <div className="App">
-    
+  <div>
+  <NavBar/>
+   </div>
+
+<div>
       <Footer/>
+</div>
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,10 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 import './App.css';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FaBeer } from 'react-icons/fa';
+import { FaBeer, FaEnvelope , FaTwitter , FaFacebook } from 'react-icons/fa';
 import {FiFacebook} from 'react-icons/fi';
+
+
 
 
 
@@ -11,12 +13,12 @@ import {FiFacebook} from 'react-icons/fi';
   //  <IconContext.Provider value={{ className: 'react-icons' }}>
 const Footer = () => (
   <section id="sidebar">
-    <section id="footer">
-      <ul className="icons">
-          <li>
-  <FaBeer />   <FiFacebook/>   <FiFacebook/>
-          </li>
-      </ul>
+    <section className="footer">
+
+          <a>
+<FaEnvelope />  <FiFacebook/>  <FaTwitter/>  <FaFacebook/>
+          </a>
+
       <p className="copyright">&copy; GigabyteCrippler</p>
     </section>
   </section>
